52009fed85f7568aad03ffab2aa20fa2c1f57543
nam <nam98nd16@gmail.com>
2020-06-17 19:41:45 +0700

    check expired token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
+import jwt_decode from "jwt-decode";
 export default function({ store, redirect }) {
-  if (!localStorage.getItem("token")) {
+  let storedUser = jwt_decode(localStorage.getItem("token"));
+  let isExpiredToken = storedUser.exp <= new Date().getTime() / 1000;
+  if (!storedUser || isExpiredToken) {
     return redirect("/login");
   }
 }
