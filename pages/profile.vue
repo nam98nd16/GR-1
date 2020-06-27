e84920eeab0a65bf447bbca8baeeecdb91d79a68
nam <nam98nd16@gmail.com>
2020-06-27 11:38:40 +0700

    update profile page's ui

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,46 +1,58 @@
 <template>
-  <div class="container">
-    <a-form :form="form" class="login-form" @submit="handleSubmit">
-      <a-form-item :label="'Email'" v-bind="formItemLayout">
-        <a-input disabled :value="currentUser ? currentUser.email : ''" placeholder="Email">
-          <a-icon slot="prefix" type="mail" style="color: rgba(0,0,0,.25)" />
-        </a-input>
-      </a-form-item>
-      <a-form-item :label="'Full name'" v-bind="formItemLayout">
-        <a-input
-          v-decorator="[
+  <a-spin :spinning="pageLoading">
+    <div class="container">
+      <a-form style="margin-top: 50px" :form="form" class="login-form" @submit="handleSubmit">
+        <a-form-item :label="'Email'" v-bind="formItemLayout">
+          <a-input disabled :value="currentUser ? currentUser.email : ''" placeholder="Email">
+            <a-icon slot="prefix" type="mail" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item :label="'Full name'" v-bind="formItemLayout">
+          <a-input
+            v-decorator="[
           'fullName',
           { rules: [{ required: true, message: 'Please input your full name!' }] },
         ]"
-          placeholder="Full name"
-        >
-          <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
-        </a-input>
-      </a-form-item>
-      <a-form-item :label="'Phone number'" v-bind="formItemLayout">
-        <a-input
-          v-decorator="[
+            placeholder="Full name"
+          >
+            <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item :label="'Phone number'" v-bind="formItemLayout">
+          <a-input
+            v-decorator="[
           'phoneNumber',
           { rules: [{ pattern: /^(0|[1-9][0-9]*)$/, message: 'Please input a valid phone number!' }] },
         ]"
-          placeholder="Phone number"
-        >
-          <a-icon slot="prefix" type="mobile" style="color: rgba(0,0,0,.25)" />
-        </a-input>
-      </a-form-item>
-      <a-form-item :label="'Birthday'" v-bind="formItemLayout">
-        <a-date-picker format="YYYY-MM-DD" v-decorator="['birthday']" />
-      </a-form-item>
-      <a-form-item :label="'  '" v-bind="formItemLayout" :colon="false">
-        <a-button
-          :loading="loading"
-          type="primary"
-          html-type="submit"
-          class="login-form-button"
-        >Update</a-button>
-      </a-form-item>
-    </a-form>
-  </div>
+            placeholder="Phone number"
+          >
+            <a-icon slot="prefix" type="mobile" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item :label="'Birthday'" v-bind="formItemLayout">
+          <a-date-picker format="YYYY-MM-DD" v-decorator="['birthday']" />
+        </a-form-item>
+        <a-form-item :label="'Gender'" v-bind="formItemLayout">
+          <a-radio-group name="gender" v-decorator="['gender', {initialValue:1}]">
+            <a-radio :value="1">Male</a-radio>
+            <a-radio :value="2">Female</a-radio>
+            <a-radio :value="3">Other</a-radio>
+          </a-radio-group>
+        </a-form-item>
+        <a-form-item :label="'About me'" v-bind="formItemLayout">
+          <a-textarea v-decorator="['about']" :autosize="{ minRows: 2, maxRows: 10 }"></a-textarea>
+        </a-form-item>
+        <a-form-item :label="'  '" v-bind="formItemLayout" :colon="false">
+          <a-button
+            :loading="loading"
+            type="primary"
+            html-type="submit"
+            class="login-form-button"
+          >Update</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </a-spin>
 </template>
 
 <script>
@@ -56,13 +68,14 @@ export default {
           sm: 24
         },
         wrapperCol: {
-          md: 16,
+          md: 8,
           sm: 24
         }
       },
       user: null,
       currentUser: null,
-      loading: false
+      loading: false,
+      pageLoading: false
     };
   },
   beforeCreate() {
@@ -89,7 +102,9 @@ export default {
               phoneNumber: values.phoneNumber ? values.phoneNumber : "",
               birthday: values.birthday
                 ? this.$moment(values.birthday).format("YYYY-MM-DD")
-                : ""
+                : "",
+              gender: values.gender,
+              about: values.about ? values.about : ""
             })
             .then(() => {
               this.$notification.success({ message: "Update successfully!" });
@@ -105,6 +120,7 @@ export default {
       });
     },
     getProfile() {
+      this.pageLoading = true;
       var docRef = this.$fireStore
         .collection("users")
         .doc(this.currentUser.user_id);
@@ -128,6 +144,9 @@ export default {
         })
         .catch(function(error) {
           console.log("Error getting document:", error);
+        })
+        .finally(() => {
+          this.pageLoading = false;
         });
     }
   }
@@ -136,9 +155,7 @@ export default {
 
 <style scoped>
 .container {
-  margin: 0 auto;
   min-height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
 }
