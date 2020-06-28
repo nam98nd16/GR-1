25ca68b7ff7f364758c9fc444693740ae062d7e0
nam <nam98nd16@gmail.com>
2020-06-28 16:05:55 +0700

    add self-assess submission

--- pages/self-assess/index.vue
+++ pages/self-assess/index.vue
@@ -19,10 +19,12 @@
       <a-collapse-panel key="3" header="Project Management Skills"></a-collapse-panel>
     </a-collapse>
     <a-modal
+      v-if="visible"
       :title="skillTitle"
       :visible="visible"
       :centered="true"
       :width="800"
+      :okButtonProps="{props: { loading: submitting }}"
       @ok="handleOk"
       @cancel="handleCancel"
     >
@@ -30,7 +32,7 @@
         <a-form-item label="Experience" v-bind="formItemLayout">
           <a-radio-group
             @change="handleExperienceSelection"
-            :value="selectedExperience[skillTitle]"
+            :value="selectedExperiences[skillTitle]"
           >
             <a-radio
               v-for="experience in experienceOptions"
@@ -40,7 +42,7 @@
           </a-radio-group>
         </a-form-item>
         <a-form-item label="Level" v-bind="formItemLayout">
-          <a-radio-group @change="handleLevelSelection" :value="selectedLevel[skillTitle]">
+          <a-radio-group @change="handleLevelSelection" :value="selectedLevels[skillTitle]">
             <a-radio v-for="level in levelOptions" :value="level" :key="level">{{level}}</a-radio>
           </a-radio-group>
         </a-form-item>
@@ -68,14 +70,18 @@ export default {
       activeKey: ["1"],
       visible: false,
       skillTitle: "",
-      selectedExperience: {},
+      selectedExperiences: {},
       experienceOptions: [],
-      selectedLevel: {},
-      levelOptions: []
+      selectedLevels: {},
+      levelOptions: [],
+      submitting: false,
+      submittedRecords: null,
+      assessmentRecordsRef: null
     };
   },
   mounted() {
     this.getEvtSkills();
+    this.retrieveAssessmentRecord();
   },
   methods: {
     getEvtSkills() {
@@ -98,6 +104,20 @@ export default {
           this.pageLoading = false;
         });
     },
+    async retrieveAssessmentRecord() {
+      this.assessmentRecordsRef = this.$fireStore.collection(
+        "assessment_records"
+      );
+      this.submittedRecords = await this.assessmentRecordsRef
+        .where("creatorId", "==", this.$fireAuth.currentUser.uid)
+        .where("assessmentEventId", "==", this.$route.params.evt.id)
+        .get();
+      if (!this.submittedRecords.empty) {
+        let record = this.submittedRecords.docs[0].data();
+        this.selectedExperiences = record.selectedExperiences;
+        this.selectedLevels = record.selectedLevels;
+      }
+    },
     openAssessModal(skill) {
       this.skillTitle = skill.title;
       if (skill.assessmentMetrics.experiences)
@@ -106,17 +126,40 @@ export default {
         this.levelOptions = skill.assessmentMetrics.levels;
       this.visible = true;
     },
-    handleOk() {
-      this.visible = false;
+    async handleOk() {
+      this.submitting = true;
+
+      let recordRef = !this.submittedRecords.empty
+        ? this.assessmentRecordsRef.doc(this.submittedRecords.docs[0].id)
+        : this.assessmentRecordsRef.doc();
+      recordRef
+        .set({
+          id: recordRef.id,
+          assessmentEventId: this.$route.params.evt.id,
+          creatorId: this.$fireAuth.currentUser.uid,
+          selectedLevels: this.selectedLevels,
+          selectedExperiences: this.selectedExperiences
+        })
+        .then(() => {
+          console.log("Document successfully updated!");
+        })
+        .catch(error => {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        })
+        .finally(() => {
+          this.submitting = false;
+          this.visible = false;
+        });
     },
     handleCancel() {
       this.visible = false;
     },
     handleExperienceSelection(event) {
-      this.$set(this.selectedExperience, this.skillTitle, event.target.value);
+      this.$set(this.selectedExperiences, this.skillTitle, event.target.value);
     },
     handleLevelSelection(event) {
-      this.$set(this.selectedLevel, this.skillTitle, event.target.value);
+      this.$set(this.selectedLevels, this.skillTitle, event.target.value);
     }
   }
 };
